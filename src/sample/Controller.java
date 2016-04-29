cc4bdaaad74a3e84798696ec2bce97153c209899
Jonathan Klaiber <joni.klaiber@gmx.de>
2016-04-30 01:29:27 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    Label titleText;
+
+    @FXML
+    TableView myTable;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setTitle();
+        TableColumn col = new TableColumn();
+        col.setCellFactory(
+            new PropertyValueFactory<String,String>("Hello")
+        );
+
+        myTable.getColumns().add(col);
+    }
+
+    public void setTitle(){
+        titleText.setText("ESL PRO MASTERS");
+    }
+}
